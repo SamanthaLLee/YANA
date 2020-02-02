baf2a859f92da15e49bba69f74e096720fb30e90
Samantha Lee <sll155@rutgers.edu>
2020-02-01 19:29:48 -0500

    Update contentscript.js

--- contentscript.js
+++ contentscript.js
@@ -1,21 +1,20 @@
+const config =
+		{
+			'keyword1' : 'I',
+			'keyword2' : 'me',
+			'keyword3' : 'myself'
+		};
+
 function saveKeyboardEvent(e)
 {
   console.log(e);
+  match();
+}
+
+function match(input, obj)
+{
+  var matched = Object.keys(obj).find(key => input.toLowerCase().search(key) > -1);
+  return obj[matched] || null;
 }
 
 document.addEventListener('keydown', function (e){saveKeyboardEvent(e);}, false);
-
-const config = 
-		{
-			'keyword1' = 'I',
-			'keyword2' = 'me',
-			'keyword3' = 'myself',
-		};
-
-		function match(input, obj)
-		{
-			var matched = Object.keys(obj).find(key => input.toLowerCase().search(key) > -1);
-			return obj[matched] || null;
-		}
-
-console.log(match())
