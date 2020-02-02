0fdb1eb079bf3b5d75b8216541a8c110b3c3e31c
shambhavir <55058421+shambhavir@users.noreply.github.com>
2020-02-01 19:21:11 -0500

    Update contentscript.js

--- contentscript.js
+++ contentscript.js
@@ -4,3 +4,18 @@ function saveKeyboardEvent(e)
 }
 
 document.addEventListener('keydown', function (e){saveKeyboardEvent(e);}, false);
+
+const config = 
+		{
+			'keyword1' = 'I',
+			'keyword2' = 'me',
+			'keyword3' = 'myself',
+		};
+
+		function match(input, obj)
+		{
+			var matched = Object.keys(obj).find(key => input.toLowerCase().search(key) > -1);
+			return obj[matched] || null;
+		}
+
+console.log(match())
