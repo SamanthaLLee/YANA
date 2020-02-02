53ffe14d189a5ea7f2083d027d53ca84982e20f1
Samantha Lee <sll155@rutgers.edu>
2020-02-01 20:08:29 -0500

    Update contentscript.js

--- contentscript.js
+++ contentscript.js
@@ -23,24 +23,6 @@ document.getElementsByTagName('input').addEventListener("click", function (){
   alert("Hi");
 });
 
-<<<<<<< HEAD
-//document.getElementById("myBtn").addEventListener("click", displayDate);
-
-const config = 
-		{
-			'keyword1' = 'I',
-			'keyword2' = 'me',
-			'keyword3' = 'myself',
-		};
-
-		function match(input, obj)
-		{
-			var matched = Object.keys(obj).find(key => input.toLowerCase().search(key) > -1);
-			return obj[matched] || null;
-		}
-
-console.log(match())
-=======
 // let input = document.querySelector('input');
 // let log = document.getElementById('log');
 //
@@ -50,4 +32,3 @@ console.log(match())
 //   log.textContent = `The field's value is
 //       ${e.target.value.length} character(s) long.`;
 // }
->>>>>>> dbe082cb3ea0ef20cdd392712cf42b5b92838780
