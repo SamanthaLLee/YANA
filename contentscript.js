dbe082cb3ea0ef20cdd392712cf42b5b92838780
Samantha Lee <sll155@rutgers.edu>
2020-02-01 20:04:00 -0500

    Update contentscript.js

--- contentscript.js
+++ contentscript.js
@@ -1,20 +1,34 @@
-const config =
-		{
-			'keyword1' : 'I',
-			'keyword2' : 'me',
-			'keyword3' : 'myself'
-		};
+// const config =
+// 		{
+// 			'keyword1' : 'I',
+// 			'keyword2' : 'me',
+// 			'keyword3' : 'myself'
+// 		};
+//
+// function saveKeyboardEvent(e)
+// {
+//   console.log(e);
+//   match();
+// }
+//
+// function match(input, obj)
+// {
+//   var matched = Object.keys(obj).find(key => input.toLowerCase().search(key) > -1);
+//   return obj[matched] || null;
+// }
+//
+// document.addEventListener('keydown', function (e){saveKeyboardEvent(e);}, false);
 
-function saveKeyboardEvent(e)
-{
-  console.log(e);
-  match();
-}
+document.getElementsByTagName('input').addEventListener("click", function (){
+  alert("Hi");
+});
 
-function match(input, obj)
-{
-  var matched = Object.keys(obj).find(key => input.toLowerCase().search(key) > -1);
-  return obj[matched] || null;
-}
-
-document.addEventListener('keydown', function (e){saveKeyboardEvent(e);}, false);
+// let input = document.querySelector('input');
+// let log = document.getElementById('log');
+//
+// input.oninput = handleInput;
+//
+// function handleInput(e) {
+//   log.textContent = `The field's value is
+//       ${e.target.value.length} character(s) long.`;
+// }
